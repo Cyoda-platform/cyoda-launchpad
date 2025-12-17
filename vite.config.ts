f4453178d2e266d3e3ed8150e090af94525c36b8
patrick-cyoda <patrick.stanton@cyoda.com>
2025-12-16 22:04:42 -0400

    move the blog posts to the public folder and add the indexing for LLMs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
@@ -13,8 +12,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
     sitemap({
       hostname: 'https://cyoda.com',
       routes: ['/', '/blog'],
